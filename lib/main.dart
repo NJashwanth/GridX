118ebc8f308cfb4b833a733c19b85a0b2c6ac6f4
JASHWANTH NEELA <jashwanthn066@gmail.com>
2026-03-01 21:36:38 -0500

    Integrate lifecycle_logger into app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter/widgets.dart';
+import 'package:lifecycle_logger/lifecycle_logger.dart';
 
 import 'app/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  LifecycleLogger.attach();
   runApp(const ProviderScope(child: MyApp()));
 }
 
